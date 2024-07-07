5f995c9180d3c5aaa56d4e2b7fc2486328e7c857
Fred Graybeal <93329347+fredgraybealSD@users.noreply.github.com>
2024-07-07 14:05:11 -0400

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,9 +20,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Social Driver', // Usually your GitHub org/user name.
+  organizationName: 'socialdriver', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
